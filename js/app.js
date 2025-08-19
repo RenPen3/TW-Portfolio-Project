693d2d514db8e3ec61dfcbafe26a0ab51105507a
René <98562029+RenPen3@users.noreply.github.com>
2025-08-18 22:51:12 -0700

    I added another image to the about section.

--- js/app.js
+++ js/app.js
@@ -1,21 +1,24 @@
 import { cards } from "/js/cards.js";
 
-const conHTML = cards.map(function (card) {
-  return ` <div class="overflow-hidden border-1 border-white hover:border-1 hover:border-black duration-700 ease-in-out rounded-md hover:shadow-lg">
-            <a href="#" class="bg-zinc-400">
+const conHTML = cards
+  .map(function (card) {
+    return ` <div class="overflow-hidden border-1 border-white hover:border-1 hover:border-black duration-700 ease-in-out rounded-md hover:shadow-lg">
+            <a href="${card.linkURL}" class="bg-zinc-400 z-1" target="_blank">
               <img
-                class="hover:scale-104 duration-700 ease-in-out z-0 object-cover w-full h-120"
+                class="block hover:scale-104 duration-700 ease-in-out z-0 object-cover w-full h-120"
                 src="${card.image}"
                 alt="image 1"
               />
             </a>
-            <div class="p-5 bg-zinc-100">
-              <h3 class="text-xl font-bold">${card.title}</h3>
-              <h4 class="text-md font-600 mb-2">
+            <div class="p-5 bg-zinc-100 z-5 text-left">
+              <h3 class="text-2xl font-black">${card.title}</h3>
+              <h4 class="text-md font-bold mb-2 text-zinc-500">
                 ${card.subTitle}
               </h4>
+                <p class="font-400 text-zinc-500">${card.description}</p>
             </div>
           </div>`;
-});
+  })
+  .join("");
 
-document.getElementById("con").innerHTML = conHTML.join("");
+document.getElementById("con").innerHTML = conHTML;
--- js/cards.js
+++ js/cards.js
@@ -3,15 +3,24 @@ export const cards = [
     image: "https://i.ibb.co/nLystPM/dod-image-1.jpg",
     title: "Love2x",
     subTitle: "Shopify eCommerce Website",
+    description:
+      "Designed the storefront layout ensuring a clean, user-friendly shopping experience.",
+    linkURL: "https://rdperez31.myshopify.com/",
   },
   {
     image: "https://i.ibb.co/qYX3NNwM/drone-thumb.webp",
     title: "RDP Drone Media",
     subTitle: "Drone Service Website",
+    description:
+      "Built with Astro, it delivers fast performance and an optimized viewing experience.",
+    linkURL: "https://rdpdronemedia.com/",
   },
   {
     image: "https://i.ibb.co/RGpJgQQk/onnit-img.webp",
     title: "Onnit Replica Project",
     subTitle: "Fitness Website",
+    description:
+      "A replica website showcasing my ability to recreate existing designs with detail.",
+    linkURL: "https://onnit.netlify.app/",
   },
 ];
